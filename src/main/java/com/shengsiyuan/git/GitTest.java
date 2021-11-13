c4e905b66b8a28daea065952783e704117b6416f
ly-zhangwl <zhangwl@szlanyou.com>
2021-11-13 17:44:07 +0800

    firsst commit

---
+++ src/main/java/com/shengsiyuan/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.shengsiyuan.git;
+
+/**
+ * @Classname GitTest
+ * @Description 类型说明
+ * @Date 2021/11/13 17:38
+ * @Created by ly-zhangwl
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
